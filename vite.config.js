db4ac303ab1c88de9adda13db6e87b6edb2ceb0f
Kyrylo Kolesnichenko <kykocq@gmail.com>
2026-03-05 22:40:00 -0500

    name fix

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(), react()],
+    base: '/data-science-project-page/',
+    plugins: [tailwindcss(), react()],
 })
